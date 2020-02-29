6ae3fdd3fc344c978c94c4799714692e3d8899f5
jadon <jadonwolffs@gmail.com>
2020-02-29 15:58:05 +0200

    added button, state and toggle

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [state,setState] = useState(`Open up App.js to start working on your app!`)
+  const [changed,setChanged] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{changed ? state : `New text!`}</Text>
+      <Text>Testing text...</Text>
+      <Text/>
+      <Button title="Change!" onPress={()=>{setChanged(!changed)}}/>
     </View>
   );
 }
